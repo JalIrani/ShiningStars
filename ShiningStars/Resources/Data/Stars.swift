9b1747f2ad42f55e570ea6747cfd43227a7c29e5
Jal Irani <jalirani@Jal-Iranis-MacBook-Pro.local>
2019-01-10 22:32:18 -0500

    detailed view started

--- ShiningStars/Resources/Data/Stars.swift
+++ ShiningStars/Resources/Data/Stars.swift
@@ -8,3 +8,8 @@
 
 import Foundation
 
+
+var stars = [
+    ["name": "steph", "picture": "me.jpg", "quote": "I love life", "bio": "I am 26 years old and for some reason I love quitting jobs and getting new ones then quitting them too. Maybe one day I will have a job that I like and I will hold it down\n Hobbies: Just chillin\n Heroes: My mom and dad"],
+    ["name": "steph", "picture": "me.jpg", "quote": "I love life", "bio": "I am 26 years old and for some reason I love quitting jobs and getting new ones then quitting them too. Maybe one day I will have a job that I like and I will hold it down\n Hobbies: Just chillin\n Heroes: My mom and dad"]
+]
